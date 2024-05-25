5c52859455d8fcdb5a231154f850e7b374d6f5e1
shikaan <spagnolo.manu@gmail.com>
2024-05-25 05:49:17 +0200

    feat: slide in the score bar

--- src/ui/HUD.cpp
+++ src/ui/HUD.cpp
@@ -1,5 +1,6 @@
 #include "events/events.h"
 #include <cstdio>
+#include <latebit/core/events/EventStep.h>
 #include <latebit/core/graphics/Colors.h>
 #include <latebit/core/graphics/DisplayManager.h>
 #include <latebit/core/objects/Object.h>
@@ -22,20 +23,28 @@ public:
     setType("HUD");
     setSolidness(SPECTRAL);
     setAltitude(lb::MAX_ALTITUDE);
-    subscribe(SCORE_UPDATED_EVENT);
     vector<Color> content;
     for (int i = 0; i < WIDTH * HEIGHT; i++)
       content.push_back(BLACK);
     this->cell = Frame(WIDTH, HEIGHT, content);
+    // The position represents the bottom left corner.
+    // This is used to allow the slide-in effect for the score bar
+    this->setPosition(Vector(0, 0));
+
+    subscribe(SCORE_UPDATED_EVENT);
+    subscribe(STEP_EVENT);
   }
 
   int draw() {
     char score[17];
     snprintf(score, 17, "SCORE: %d", this->score);
 
-    int result = this->cell.draw(Vector());
-    result += DM.drawString(Vector(DM.getHorizontalCells() / 2, 4), score,
-                            TEXT_ALIGN_CENTER, WHITE);
+    auto position = this->getPosition();
+    auto topLeftCorner = position - Vector(0, HEIGHT);
+    int result = this->cell.draw(topLeftCorner);
+    result += DM.drawString(
+        Vector(DM.getHorizontalCells() / 2, 4 + topLeftCorner.getY()), score,
+        TEXT_ALIGN_CENTER, WHITE);
 
     return result;
   }
@@ -46,6 +55,19 @@ public:
       score = event->points;
       return 1;
     }
+
+    if (e->getType() == STEP_EVENT) {
+      auto event = static_cast<const EventStep *>(e);
+      auto steps = event->getStepCount();
+
+      if (this->getPosition().getY() >= HEIGHT) {
+        unsubscribe(STEP_EVENT);
+        return 1;
+      }
+
+      this->setPosition(this->getPosition() + Vector(0, 1));
+    }
+
     return 0;
   }
 };
